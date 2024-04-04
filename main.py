21e5305d495806b62cde32ebe236e9092f3750e6
Bigbolt2000 <164912624+Bigbolt2000@users.noreply.github.com>
2024-04-04 02:51:05 -0400

    Finding Prime Numbers

---
+++ main.py
@@ -0,0 +1,11 @@
+number = [1,2]
+prime_num = []
+for e in range(1, 25):
+    if e not in number:
+        prime_num.append(e)
+    else:
+        for i in range(1, 10):
+            b = e * i
+            number.append(b)
+
+print(prime_num)
